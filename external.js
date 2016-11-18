37d3b5401a5698e6d4ccb833178c5fee477824f4
gopherproxy <gopherproxy@gmail.com>
2016-11-18 16:13:03 +0100

    with external JS

---
+++ external.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+// output til JavaScript konsolen - en feature til JS udvikler!
+var konsolenschweiss = "Per FEKT";
+console.log(konsolenschweiss);
+
+document.getElementById('clicker').onclick = function(){
+	'use strict';
+   // this function is strict...
+	alert("Jep - det funkede!");
+};
